7000e0986eb2dce69c55d326cf6ae567f9165c14
marcus <mingyen1202@gmail.com>
2019-05-13 11:26:42 +0800

    Initial Spring Boot JDBC Project

---
+++ src/main/java/com/chtti/demo/SpringBootJDBCDemo/SpringBootJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chtti.demo.SpringBootJDBCDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcDemoApplication.class, args);
+	}
+
+}
